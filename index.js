2ee65c651ad9d3e11c5d317ff20ce67ad3e4a3b2
Yoni Lerner <yonilerner@gmail.com>
2017-07-28 15:53:44 -0700

    Attempt unsuccessfully to use decorateConfig. Instead, use a hack

--- index.js
+++ index.js
@@ -9,25 +9,46 @@ const notify = (title, body) => {
     `)
 }
 
-const matchers = [
-    {
-        regex: /.*emit\b.*/,
-        title: 'Webpack',
-        body: () => new Date().toISOString()
-    },
-    {
-        regex: /.*Compilation complete. Watching for file changes*/,
-        title: 'Typescript',
-        body: line => line.split(' - ')[0]
-    }
-]
+let matchers
+exports.decorateConfig = config => {
+    return Object.assign({}, config, {
+        hyperTriggersMatchers: [
+            {
+                matcher: /.*emit\b.*/,
+                title: 'Webpack',
+                body: () => new Date().toISOString()
+            },
+            {
+                matcher: 'Compilation complete. Watching for file changes',
+                title: 'Typescript',
+                body: line => line.split(' - ')[0]
+            }
+        ]
+    })
+}
 
 exports.middleware = store => next => action => {
+    if (!matchers) {
+        matchers = exports.decorateConfig({}).hyperTriggersMatchers
+        if (e.remote.app.config.getConfig().hyperTriggersMatchers) {
+            matchers = e.remote.app.config.getConfig().hyperTriggersMatchers
+        }
+    }
     if (action.type === 'SESSION_ADD_DATA') {
         const ansiStripped = stripAnsi(action.data)
-        matchers.forEach(({regex, title, body}) => {
-            if (regex.test(ansiStripped)) {
-                notify(title, body(ansiStripped))
+        matchers.forEach(({matcher, title, body}) => {
+            if (
+                (matcher instanceof RegExp && matcher.test(ansiStripped)) ||
+                (typeof matcher === 'function' && matcher(ansiStripped)) ||
+                (typeof matcher === 'string' && ansiStripped.includes(matcher))
+            ) {
+                let bodyText
+                if (typeof body === 'function') {
+                    bodyText = body(ansiStripped, matcher)
+                } else {
+                    bodyText = body
+                }
+                notify(title, bodyText)
             }
         })
     }
